54e3b0b7eec0357bbfeae1a560e165ed50b627cf
Ashish Jain <arjain18@gmail.com>
2024-07-27 09:39:42 +0530

    Add project files.

---
+++ SmartSpent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartSpent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
